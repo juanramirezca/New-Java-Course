924a56a6618419fcf0444981dc3e8bb5bcf01f95
juararam <juan.ju.ramirez@oracle.com>
2024-08-28 15:11:19 -0600

    Adding second test

---
+++ New-Java-Course/00 Introduction/06 Loops/Main/src/LoopsMain.java
@@ -0,0 +1,5 @@
+public class LoopsMain {
+    public static void main(String[] args) {
+        System.out.println("They are here!!!");
+    }
+}
--- New-Java-Course/00 Introduction/06 Loops/MainLoops.java
+++
@@ -1,5 +0,0 @@
-public class MainLoops{
-    public static void main(String[] args) {
-        System.out.println("This is a test");
-    }
-}
